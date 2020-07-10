a33bb93f32a22c967a25d8b596cc9cc9c43cad05
Ba Thien Tran <bathientran@gmail.com>
2020-07-10 18:11:06 +0200

    adds centering

--- frontend/src/views/Pages/Auth/Register.js
+++ frontend/src/views/Pages/Auth/Register.js
@@ -1,118 +1,171 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
-import { updateAccount } from '../../../store/actions_creators';
-import { Alert, Col, Form, Button, Row } from 'react-bootstrap';
-import { Formik } from 'formik';
-import { registerSchema } from '../../../utils/validations';
-import Axios from '../../../utils/axios';
-import Container from '../../Layouts/Container';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { updateAccount } from "../../../store/actions_creators";
+import { Alert, Col, Form, Button, Row } from "react-bootstrap";
+import { Formik } from "formik";
+import { registerSchema } from "../../../utils/validations";
+import Axios from "../../../utils/axios";
+import Container from "../../Layouts/Container";
+import { Link } from "react-router-dom";
 
-import '../../../assets/sass/bootstrap.scss';
+import "../../../assets/sass/bootstrap.scss";
 
 const Register = (props) => {
-	const [alert, updateAlert] = useState({ type: null, message: null });
+  const [alert, updateAlert] = useState({ type: null, message: null });
 
-	function setAlert(obj) {
-		updateAlert(obj);
-		setTimeout(() => updateAlert({ type: null, message: null }), 3000);
-	}
+  function setAlert(obj) {
+    updateAlert(obj);
+    setTimeout(() => updateAlert({ type: null, message: null }), 3000);
+  }
 
-	return (
-		<React.Fragment>
-			<Container singleCol={true}>
-				{/* I can't seem to figure out the vertical alignment on this */}
-				<div className={""}>
-				<Row className={"justify-content-center"}>
-				<Col xs={3} sm={3} md={3} lg={3} className={"align-middle"}>
-					
-						<h1 className={"text-center"}>register</h1>
-						<br />
-						{alert.type !== null ? <Alert variant={alert.type}>{alert.message}</Alert> : null}
-						<Formik
-							validationSchema={registerSchema}
-							initialValues={{ username: '', email: '', password: '', cpassword: '' }}
-							onSubmit={(values, actions) => {
-								Axios.post('/auth/register', { form: values }).then((res) => {
-									updateAlert({
-										type: 'success',
-										message: `You have created a new account successfully. You'll be redireced to homepage..`
-									});
-									localStorage.setItem('loggedIn', true);
-									props.updateAccount(res.data);
-								}).catch((err) => {
-									setAlert({ type: 'danger', message: err.response.data.error });
-									actions.setSubmitting(false);
-								});
-							}}
-							render={({ values, errors, status, touched, handleBlur, handleChange, handleSubmit, isSubmitting, }) => (
-								<Form onSubmit={handleSubmit} className={"justify-content-center"}>
-									<Form.Group>
-										<Form.Control
-											type="text"
-											name="username"
-											placeholder="full name"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											value={values.username}
-										/>
-										{errors.username && touched.username && <Form.Text className="text-danger">{errors.username}</Form.Text>}
-									</Form.Group>
-									<Form.Group>
-										<Form.Control
-											type="email"
-											name="email"
-											placeholder="email"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											value={values.email}
-										/>
-										{errors.email && touched.email && <Form.Text className="text-danger">{errors.email}</Form.Text>}
-									</Form.Group>
-									<Form.Group>
-										<Form.Control
-											type="password"
-											name="password"
-											placeholder="password"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											value={values.password}
-										/>
-										{errors.password && touched.password && <Form.Text className="text-danger">{errors.password}</Form.Text>}
-									</Form.Group>
-									<Form.Group>
-										<Form.Control
-											type="password"
-											name="cpassword"
-											placeholder="confirm password"
-											onChange={handleChange}
-											onBlur={handleBlur}
-											value={values.cpassword}
-										/>
-										{errors.cpassword && touched.cpassword && <Form.Text className="text-danger">{errors.cpassword}</Form.Text>}
-									</Form.Group>
-									{/* also not sure why this button isn't vertically aligned and why the border is not showing. Probably something in _variables.scss */}
-										<Button className={"btn-group-vertical"} variant="primary" type="submit" disabled={isSubmitting}>submit</Button>
-								</Form>
-							)}
-						/>
-					<div className={"align-middle"}>
-						<p>want to log in? <Link to="/auth/login">click here.</Link></p>
-					</div>
-				</Col>
-				</Row>
-				</div>
-			</Container>
-		</React.Fragment>
-	)
-}
+  return (
+    <React.Fragment>
+      <Container singleCol={true}>
+        {/* I can't seem to figure out the vertical alignment on this */}
+        {/* <div className={""}> */}
+        <Row className={"justify-content-center"}>
+          <Col xs={3} sm={3} md={3} lg={3}>
+            <h1 className={"text-center"}>register</h1>
+            <br />
+            {alert.type !== null ? (
+              <Alert variant={alert.type}>{alert.message}</Alert>
+            ) : null}
+            <Formik
+              validationSchema={registerSchema}
+              initialValues={{
+                username: "",
+                email: "",
+                password: "",
+                cpassword: "",
+              }}
+              onSubmit={(values, actions) => {
+                Axios.post("/auth/register", { form: values })
+                  .then((res) => {
+                    updateAlert({
+                      type: "success",
+                      message: `You have created a new account successfully. You'll be redireced to homepage..`,
+                    });
+                    localStorage.setItem("loggedIn", true);
+                    props.updateAccount(res.data);
+                  })
+                  .catch((err) => {
+                    setAlert({
+                      type: "danger",
+                      message: err.response.data.error,
+                    });
+                    actions.setSubmitting(false);
+                  });
+              }}
+              render={({
+                values,
+                errors,
+                status,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                isSubmitting,
+              }) => (
+                <Form
+                  onSubmit={handleSubmit}
+                  className={"justify-content-center"}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Form.Group>
+                    <Form.Control
+                      type="text"
+                      name="username"
+                      placeholder="full name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.username}
+                    />
+                    {errors.username && touched.username && (
+                      <Form.Text className="text-danger">
+                        {errors.username}
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      placeholder="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                    />
+                    {errors.email && touched.email && (
+                      <Form.Text className="text-danger">
+                        {errors.email}
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      type="password"
+                      name="password"
+                      placeholder="password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                    />
+                    {errors.password && touched.password && (
+                      <Form.Text className="text-danger">
+                        {errors.password}
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Control
+                      type="password"
+                      name="cpassword"
+                      placeholder="confirm password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.cpassword}
+                    />
+                    {errors.cpassword && touched.cpassword && (
+                      <Form.Text className="text-danger">
+                        {errors.cpassword}
+                      </Form.Text>
+                    )}
+                  </Form.Group>
+                  {/* also not sure why this button isn't vertically aligned and why the border is not showing. Probably something in _variables.scss */}
+                  <Button
+                    className={"btn-group-vertical"}
+                    variant="primary"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    submit
+                  </Button>
+                </Form>
+              )}
+            />
+            <div style={{ textAlign: "center" }}>
+              <p>
+                want to log in? <Link to="/auth/login">click here.</Link>
+              </p>
+            </div>
+          </Col>
+        </Row>
+        {/* </div> */}
+      </Container>
+    </React.Fragment>
+  );
+};
 
-const mapDispatchToProps = dispatch => {
-	return {
-		updateAccount: (data) => {
-			dispatch(updateAccount(data))
-		}
-	};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateAccount: (data) => {
+      dispatch(updateAccount(data));
+    },
+  };
 };
 
 export default connect(null, mapDispatchToProps)(Register);
