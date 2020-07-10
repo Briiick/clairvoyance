5a7246e9fba3583632cfe544e49ec8ad5beb3c09
Ba Thien Tran <bathientran@gmail.com>
2020-07-10 18:22:12 +0200

    removes social core

--- backend/clairvoyance/settings/settings_base.py
+++ backend/clairvoyance/settings/settings_base.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_auth.registration',
     'rest_auth',
-    'social_django',
     'corsheaders',
     'api',
     'users.apps.UsersConfig',
@@ -109,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
-    'social_core.backends.google.GoogleOAuth2',
     'django.contrib.auth.backends.ModelBackend',
   
     # `allauth` specific authentication methods, such as login by e-mail
@@ -123,19 +121,6 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'g5sb_F4HpY4Ey0O7IXlR6mVb'
 
 SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'email']
 SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
-
-SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',  # <- this line not included by default
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
-)
 
 
 # Internationalization
