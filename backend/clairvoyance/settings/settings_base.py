cdb7c26259cb1113947ed3ec4a03615d65287cd4
Alexander Wu <alexanderwu2000@gmail.com>
2020-07-09 21:52:07 -0400

    Add 'social_django' to INSTALLED_APPS in backend

--- backend/clairvoyance/settings/settings_base.py
+++ backend/clairvoyance/settings/settings_base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_auth.registration',
     'rest_auth',
+    'social_django',
     'corsheaders',
     'api',
     'users.apps.UsersConfig',
